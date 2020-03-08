1bd02be99f5c0f0140424567cc23ab091a42ed6b
cagri21 <cagriyorukoglu21@gmail.com>
2020-03-08 18:15:38 +0300

    Initial Commit

---
+++ YCTabBarController/YCTabBarController.h
@@ -0,0 +1,19 @@
+//
+//  YCTabBarController.h
+//  YCTabBarController
+//
+//  Created by Çağrı Yörükoğlu on 8.03.2020.
+//  Copyright © 2020 Çağrı Yörükoğlu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YCTabBarController.
+FOUNDATION_EXPORT double YCTabBarControllerVersionNumber;
+
+//! Project version string for YCTabBarController.
+FOUNDATION_EXPORT const unsigned char YCTabBarControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YCTabBarController/PublicHeader.h>
+
+
